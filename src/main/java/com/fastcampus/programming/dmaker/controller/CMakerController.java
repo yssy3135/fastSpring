a344949f249fc2a93bde96da8b94356d3758eda3
ys313 <ys3135@gmail.com>
2021-08-26 15:48:52 +0900

    예외 처리를 제대로 안했을때의 문제점

---
+++ src/main/java/com/fastcampus/programming/dmaker/controller/CMakerController.java
@@ -0,0 +1,10 @@
+package com.fastcampus.programming.dmaker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CMakerController {
+
+
+
+}
--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -70,18 +70,7 @@ public class DMakerController {
         return dMakerService.deleteDeveloper(memberId);
     }
 
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    @ExceptionHandler
-    public DMakerErrorResponse handleException(DMakerException e,
-                                               HttpServletRequest request){
-        log.error("errorCode: {},  url: {}, message: {},",
-                e.getDMakerErrorCode(), request.getRequestURI(),e.getDetailMessage());
 
-        return DMakerErrorResponse.builder()
-                .errorCode(e.getDMakerErrorCode())
-                .errorMessage(e.getDetailMessage())
-                .build();
-    }
 
 
 
