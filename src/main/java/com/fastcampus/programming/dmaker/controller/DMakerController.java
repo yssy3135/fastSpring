9527811f330479f5702a770281f2202e6555871e
ys313 <ys3135@gmail.com>
2021-08-26 15:12:08 +0900

    글로벌 예외처리 기법_01

--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,14 +1,14 @@
 package com.fastcampus.programming.dmaker.controller;
 
-import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
-import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
-import com.fastcampus.programming.dmaker.dto.DeveloperDto;
-import com.fastcampus.programming.dmaker.dto.EditDeveloper;
+import com.fastcampus.programming.dmaker.dto.*;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -69,6 +69,21 @@ public class DMakerController {
     ){
         return dMakerService.deleteDeveloper(memberId);
     }
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    @ExceptionHandler
+    public DMakerErrorResponse handleException(DMakerException e,
+                                               HttpServletRequest request){
+        log.error("errorCode: {},  url: {}, message: {},",
+                e.getDMakerErrorCode(), request.getRequestURI(),e.getDetailMessage());
+
+        return DMakerErrorResponse.builder()
+                .errorCode(e.getDMakerErrorCode())
+                .errorMessage(e.getDetailMessage())
+                .build();
+    }
+
+
 
 
 }
--- src/main/java/com/fastcampus/programming/dmaker/repository/DeveloperRepository.java
+++ src/main/java/com/fastcampus/programming/dmaker/repository/DeveloperRepository.java
@@ -3,6 +3,7 @@ package com.fastcampus.programming.dmaker.repository;
 import com.fastcampus.programming.dmaker.code.StatusCode;
 import com.fastcampus.programming.dmaker.entity.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
